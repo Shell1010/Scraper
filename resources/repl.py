d91aa0b032642523663e05ad1f06324a35f79d3d
Shell <72198971+Shell1010@users.noreply.github.com>
2022-07-26 23:29:59 +0100

    Trying to get cookie

--- resources/repl.py
+++ resources/repl.py
@@ -3,6 +3,7 @@ import aiohttp
 import asyncio
 from aioconsole import aprint
 import websockets
+import zipfile
 
 class ReplIt:
     def __init__(self) -> None:
@@ -13,7 +14,6 @@ class ReplIt:
             "Connection": "keep-alive",
             "x-requested-with": "XMLHttpRequest"
         }
-
 
 
         self.GRAPHQL = "https://replit.com/graphql"
@@ -32,7 +32,7 @@ class ReplIt:
             {
                 "operationName":"ReplView",
                 "variables":{
-                    "url":"/@Faysxl/Windys-nitro-generator"
+                    "url":f"{repl_url}"
                 },
                 "query":"query ReplView($url: String!) {\n  repl(url: $url) {\n    ... on Repl {\n      id\n      imageUrl\n      ...ReplViewRepl\n      __typename\n    }\n    __typename\n  }\n  currentUser {\n    id\n    ...ReplViewCurrentUser\n    __typename\n  }\n}\n\nfragment ReplViewRepl on Repl {\n  id\n  title\n  timeCreated\n  imageUrl\n  publicReleasesForkCount\n  publicForkCount\n  owner {\n    ... on Team {\n      id\n      username\n      url\n      image\n      __typename\n    }\n    ... on User {\n      id\n      username\n      url\n      image\n      __typename\n    }\n    __typename\n  }\n  relatedRepls(limitPerGroup: 3) {\n    name\n    repls {\n      id\n      publishedAs\n      ...ReplLinkRepl\n      ...TemplateReplCardRepl\n      ...ReplPostReplCardRepl\n      __typename\n    }\n    __typename\n  }\n  lang {\n    id\n    displayName\n    __typename\n  }\n  currentUserPermissions {\n    containerWrite\n    publish\n    changeIconUrl\n    __typename\n  }\n  publishedAs\n  deployment {\n    id\n    activeRelease {\n      id\n      timeCreated\n      __typename\n    }\n    __typename\n  }\n  ...ReplViewReplTitleRepl\n  ...ReplViewReplViewerRepl\n  ...ReplLinkRepl\n  ...ReplViewFooterRepl\n  __typename\n}\n\nfragment ReplLinkRepl on Repl {\n  id\n  url\n  nextPagePathname\n  __typename\n}\n\nfragment TemplateReplCardRepl on Repl {\n  id\n  iconUrl\n  templateCategory\n  title\n  description(plainText: true)\n  publicReleasesForkCount\n  templateLabel\n  likeCount\n  url\n  owner {\n    ... on User {\n      id\n      ...TemplateReplCardFooterUser\n      __typename\n    }\n    ... on Team {\n      id\n      ...TemplateReplCardFooterTeam\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment TemplateReplCardFooterUser on User {\n  id\n  username\n  image\n  url\n  __typename\n}\n\nfragment TemplateReplCardFooterTeam on Team {\n  id\n  username\n  image\n  url\n  __typename\n}\n\nfragment ReplPostReplCardRepl on Repl {\n  id\n  iconUrl\n  description(plainText: true)\n  ...ReplPostReplInfoRepl\n  ...ReplStatsRepl\n  ...ReplLinkRepl\n  tags {\n    id\n    ...PostsFeedNavTag\n    __typename\n  }\n  owner {\n    ... on Team {\n      id\n      username\n      url\n      image\n      __typename\n    }\n    ... on User {\n      id\n      username\n      url\n      image\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment ReplPostReplInfoRepl on Repl {\n  id\n  title\n  description(plainText: true)\n  imageUrl\n  iconUrl\n  templateInfo {\n    label\n    iconUrl\n    __typename\n  }\n  __typename\n}\n\nfragment ReplStatsRepl on Repl {\n  id\n  likeCount\n  runCount\n  commentCount\n  __typename\n}\n\nfragment PostsFeedNavTag on Tag {\n  id\n  isOfficial\n  __typename\n}\n\nfragment ReplViewReplTitleRepl on Repl {\n  id\n  title\n  iconUrl\n  templateInfo {\n    iconUrl\n    __typename\n  }\n  owner {\n    ... on User {\n      id\n      username\n      __typename\n    }\n    ... on Team {\n      id\n      username\n      __typename\n    }\n    __typename\n  }\n  ...ReplViewReplActionsPermissions\n  __typename\n}\n\nfragment ReplViewReplActionsPermissions on Repl {\n  id\n  lastPublishedAt\n  publishedAs\n  templateReview {\n    id\n    promoted\n    __typename\n  }\n  currentUserPermissions {\n    publish\n    __typename\n  }\n  ...UnpublishReplRepl\n  __typename\n}\n\nfragment UnpublishReplRepl on Repl {\n  id\n  commentCount\n  likeCount\n  runCount\n  publishedAs\n  __typename\n}\n\nfragment ReplViewReplViewerRepl on Repl {\n  id\n  publishedAs\n  runCount\n  publicForkCount\n  publicReleasesForkCount\n  prodUrl: hostedUrl(dotty: true)\n  isProject\n  nextPagePathname\n  lang {\n    id\n    header\n    displayName\n    __typename\n  }\n  ...ReplViewerOutputOverlayRepl\n  ...UseReplViewerRepl\n  ...LikeButtonRepl\n  __typename\n}\n\nfragment ReplViewerOutputOverlayRepl on Repl {\n  id\n  title\n  imageUrl\n  lastPublishedAt\n  currentUserPermissions {\n    changeImageUrl\n    __typename\n  }\n  __typename\n}\n\nfragment UseReplViewerRepl on Repl {\n  id\n  previewUrl: hostedUrl(dotty: false, dev: false)\n  url\n  wasPosted\n  wasPublished\n  publishedAs\n  isProject\n  lang {\n    id\n    canUseShellRunner\n    hasReplboxWebview\n    __typename\n  }\n  config {\n    isServer\n    isVnc\n    __typename\n  }\n  deployment {\n    id\n    activeRelease {\n      id\n      previewUrl: hostedUrl\n      __typename\n    }\n    __typename\n  }\n  replViewSettings {\n    id\n    defaultView\n    replFile\n    __typename\n  }\n  ...CrosisContextRepl\n  __typename\n}\n\nfragment CrosisContextRepl on Repl {\n  id\n  language\n  slug\n  user {\n    id\n    username\n    __typename\n  }\n  currentUserPermissions {\n    containerWrite\n    __typename\n  }\n  flagOwnerDotReplitPackager: gateOnOwner(feature: \"flag-dotreplit-packager\")\n  __typename\n}\n\nfragment LikeButtonRepl on Repl {\n  id\n  currentUserDidLike\n  likeCount\n  url\n  wasPosted\n  wasPublished\n  __typename\n}\n\nfragment ReplViewFooterRepl on Repl {\n  id\n  description\n  lastPublishedAt\n  publishedAs\n  deployment {\n    id\n    activeRelease {\n      id\n      timeCreated\n      __typename\n    }\n    __typename\n  }\n  owner {\n    ... on Team {\n      id\n      username\n      url\n      image\n      followerCount\n      isFollowedByCurrentUser\n      __typename\n    }\n    ... on User {\n      id\n      username\n      url\n      image\n      followerCount\n      isFollowedByCurrentUser\n      __typename\n    }\n    __typename\n  }\n  source {\n    release {\n      id\n      __typename\n    }\n    deployment {\n      id\n      repl {\n        id\n        ...ReplViewSourceRepl\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n  tags {\n    id\n    __typename\n  }\n  origin {\n    id\n    ...ReplViewSourceRepl\n    __typename\n  }\n  __typename\n}\n\nfragment ReplViewSourceRepl on Repl {\n  id\n  iconUrl\n  title\n  templateLabel\n  ...ReplLinkRepl\n  owner {\n    ... on Team {\n      id\n      username\n      url\n      image\n      __typename\n    }\n    ... on User {\n      id\n      username\n      url\n      image\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment ReplViewCurrentUser on CurrentUser {\n  id\n  username\n  isSubscribed\n  isModerator: hasRole(role: MODERATOR)\n  isAdmin: hasRole(role: ADMIN)\n  ...ReplViewReplViewerCurrentUser\n  __typename\n}\n\nfragment ReplViewReplViewerCurrentUser on CurrentUser {\n  id\n  ...LikeButtonCurrentUser\n  ...CrosisContextCurrentUser\n  __typename\n}\n\nfragment LikeButtonCurrentUser on CurrentUser {\n  id\n  isVerified\n  __typename\n}\n\nfragment CrosisContextCurrentUser on CurrentUser {\n  id\n  username\n  isSubscribed\n  flagTrackOtClientDataLoss: gate(feature: \"flag-ot-data-loss-client-tracking\")\n  flagPid1Ping: gate(feature: \"flag-pid1-ping-sample\")\n  flagNoPongReconnect: gate(feature: \"flag-no-pong-reconnect\")\n  __typename\n}\n"
             }
@@ -81,9 +81,7 @@ class ReplIt:
                     await aprint(f"{j}")
 
 
-    async def get_zip(self):
-        # In order to get zip we need to first somehow get the cookie thats related to being authenticated
-        # My balls hurt uwu
+    async def get_zip(self, repl_url: str):
         headers = {
             "Accept": "text/html,application/xhtml+xml",
             "sec-ch-ua": '"Chromium";v="102", "Not A;Brand";v="99"',
@@ -91,22 +89,38 @@ class ReplIt:
             "sec-ch-ua-platform": "Linux",
             "Service-Worker-Navigation-Preload": "true",
             "Upgrade-Insecure-Requests": "1",
-            "Cookie": "_anon_id=abf0f575-1c1c-4de7-bc67-7803533ea9b5; connect.sid=s%3A0EXIP7bz-8-ze2sn-IPl2UixD9vfqAW5.La8BiTk8r4f5LeVffnTW1yuAUrqApPD0%2B3ow6a4BYtg; replit:authed=1; replit_authed=1; sidebarClosed=true; replit_ng=1658857640.606.45.139165|8035451343a2d8f3e54599c71b2aec19; amplitudeSessionId=1658857866; _dd_s=logs=1&id=d872676e-4637-4660-b42c-d0e961f8d4bd&created=1658855108078&expire=1658858774083&rum=0",
+            "Cookie": "_anon_id=abf0f575-1c1c-4de7-bc67-7803533ea9b5; connect.sid=s%3A0EXIP7bz-8-ze2sn-IPl2UixD9vfqAW5.La8BiTk8r4f5LeVffnTW1yuAUrqApPD0%2B3ow6a4BYtg; replit:authed=1; replit_authed=1; sidebarClosed=true; replit_ng=1658866863.878.1158.975121|8035451343a2d8f3e54599c71b2aec19; amplitudeSessionId=1658866864; _dd_s=logs=1&id=6f96cf29-fa31-4032-8e7d-6a518088db97&created=1658866865100&expire=1658867965634",
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.167 Safari/537.36"
         }
         async with aiohttp.ClientSession() as session:
-            async with session.get("https://replit.com/@GoodVessel92551/Parkour-Game.zip", headers=headers) as resp:
+        # In order to get zip we need to first somehow get the cookie thats related to being authenticated
+        # My balls hurt uwu
+            async with session.get("https://replit.com", headers=headers) as resp:
+                if resp.status == 200:
+                    cookie = resp.headers
+                    await aprint(cookie)
+
+
+
+
+
+            async with session.get("https://replit.com/{repl_url}", headers=headers) as resp:
                 if resp.status == 200:
                     j = await resp.read()
-                    with open('./something.zip', 'wb') as f:
+                    with open('./resources/data/repl.zip', 'wb') as f:
                         f.write(j)
                 else:
                     j = await resp.text()
                     await aprint(resp.status)
 
+    async def search_zip(self):
+        with zipfile.ZipFile('./resources/data/repl.zip', 'r') as zip_ref:
+            zip_ref.extractall('./resources/data')
+
+
 
 
 if __name__ == "__main__":
     repl = ReplIt()
-    url= asyncio.run(repl.get_zip())
-    print(url)
+    asyncio.run(repl.get_zip("@Faysxl/Windys-nitro-generator"))
+    asyncio.run(repl.search_zip())
